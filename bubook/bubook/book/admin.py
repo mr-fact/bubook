e15363fd11a05f63e120e64e1163f7a8933f9158
mr-fact <mr.fact.202020@gmail.com>
2024-06-26 19:54:25 +0330

    Add admin classes and register models in the admin panel

--- bubook/bubook/book/admin.py
+++ bubook/bubook/book/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from bubook.book.models import Tag, Book, Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
