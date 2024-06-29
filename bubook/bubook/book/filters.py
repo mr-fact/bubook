9b5b0343f832164e103edf5832f5b898749945ae
mr-fact <mr.fact.202020@gmail.com>
2024-06-29 21:03:57 +0330

    create new category filterset

---
+++ bubook/bubook/book/filters.py
@@ -0,0 +1,10 @@
+import django_filters
+from django_filters import FilterSet
+
+
+class CategoryFilter(FilterSet):
+    name = django_filters.CharFilter(lookup_expr='icontains')
+    parent = django_filters.CharFilter(method='filter_parent')
+
+    def filter_parent(self, queryset, name, value):
+        return queryset.filter(parent__name=value)
