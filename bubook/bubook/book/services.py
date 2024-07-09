3824d3d00ca01a632727f8d2f298fd58c9d4839c
mr-fact <mr.fact.202020@gmail.com>
2024-07-09 18:52:08 +0300

    create get_or_create_category, get_or_create_tag, and create_book services

--- bubook/bubook/book/services.py
+++ bubook/bubook/book/services.py
@@ -1,4 +1,6 @@
-from bubook.book.models import Category, Tag
+from django.db import transaction
+
+from bubook.book.models import Category, Tag, Book
 
 
 def create_category(name: str, parent: str = None) -> Category:
@@ -13,6 +15,14 @@ def create_category(name: str, parent: str = None) -> Category:
     return category
 
 
+def get_or_create_category(name: str, parent: str = None) -> Category:
+    if Category.objects.filter(name=name).exists():
+        category = Category.objects.get(name=name)
+    else:
+        category = create_category(name, parent)
+    return category
+
+
 def create_tag(name: str) -> Tag:
     try:
         tag = Tag(name=name)
@@ -20,3 +30,21 @@ def create_tag(name: str) -> Tag:
     except Exception as e:
         raise ValueError('tag already exists')
     return tag
+
+
+def get_or_create_tag(name: str) -> Tag:
+    try:
+        tag = create_tag(name)
+    except Exception as e:
+        tag = Tag.objects.get(name=name)
+    return tag
+
+
+@transaction.atomic
+def create_book(name: str, price: int, category: Category, tags: list[str]) -> Book:
+    book = Book(name=name, price=price, category=category)
+    book.save()
+    for tag_name in tags:
+        tag = get_or_create_tag(tag_name)
+        book.tags.add(tag)
+    return book
