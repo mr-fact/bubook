e02d0daaf4835fd01f881c5642547ceebc2aafe5
mr-fact <mr.fact.202020@gmail.com>
2024-07-14 10:32:20 +0330

    create new task for back up redis otps to db

--- bubook/bubook/users/tasks.py
+++ bubook/bubook/users/tasks.py
@@ -1,6 +1,26 @@
 from celery import shared_task
+from django.core.cache import cache
+
+from bubook.users.models import OtpRecord
 
 
 @shared_task
 def send_otp_message(phone: str, code: str):
     print(f'sms sent. [{phone}->{code}]')
+
+
+@shared_task
+def backup_otps_to_db_task():
+    keys = cache.keys('all_otp_*')
+    otp_records = []
+    for key in keys:
+        print(f'backup otp {key} to db')
+        otp_records.append(
+            OtpRecord(
+                phone=key.split('_')[-2],
+                code=key.split('_')[-1],
+                created_at=cache.get(key)
+            )
+        )
+        cache.delete(key)
+    OtpRecord.objects.bulk_create(otp_records)
