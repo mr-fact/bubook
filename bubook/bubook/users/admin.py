0b9cabf6e7e3a14ee5c8861b23fb756ea7a98545
mr-fact <mr.fact.202020@gmail.com>
2024-07-14 10:30:41 +0330

    create otp record admin model

--- bubook/bubook/users/admin.py
+++ bubook/bubook/users/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from bubook.users.models import BaseUser
+from bubook.users.models import BaseUser, OtpRecord
 
 
 @admin.register(BaseUser)
 class BaseUserAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(OtpRecord)
+class OtpRecordAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created_at', 'phone', 'code', )
